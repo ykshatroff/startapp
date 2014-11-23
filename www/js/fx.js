c4367d939d99f8f62587a7e180a9e0b899882521
Yks <yks-uno@ya.ru>
2014-11-23 21:22:31 +0300

    js FX: new events queue
    * queue object with controlled timeout and infinity
    * fx: fadeout & animations

--- www/js/fx.js
+++ www/js/fx.js
@@ -4,25 +4,78 @@
 ;(function($) {
     var SELECTOR = "#developer-column li";
     var ANIM_SPEED = 2000;
-    var ON_ANIM_NEXT = "animation_next";
-    var animQueue = [];
+    var SHOW_DELAY = 2000;
+    var FADEOUT_COMPLETE_FLAG = 0;
 
+    var queue = {
+        EVENT_NEXT: "queue_event_next",
+        _queue: [],
+        _queueIndex: 0,
+        infinite: true,
+        timeout: 500,
 
-    function slideQueue($slider) {
+        add: function(func) {
+            queue._queue.push(func);
+            return queue;
+        },
+
+        next: function() {
+            console.log("queue.next(): " + queue._queueIndex);
+            var queueLength = queue._queue.length;
+            if (! queueLength) {
+                return;
+            }
+            // exec a function under the current index
+            queue._queue[queue._queueIndex]();
+
+            // advance index
+            queue._queueIndex ++;
+            if (queue._queueIndex >= queueLength) {
+                if (queue.infinite) {
+                    queue._queueIndex = 0;
+                } else {
+                    queue.stop();
+                }
+            }
+        },
+
+        complete: function() {
+            console.log("queue.complete(): " + queue._queueIndex);
+            setTimeout(function() {
+                $(document).trigger(queue.EVENT_NEXT);
+            }, queue.timeout);
+
+        },
+
+        start: function() {
+            $(document).on(queue.EVENT_NEXT, queue.next);
+            $(document).trigger(queue.EVENT_NEXT);
+        },
+
+        stop: function () {
+            $(document).off(queue.EVENT_NEXT);
+        }
+    };
+
+    function addToSlideQueue($slider) {
         return function() {
-            animQueue && $(document).one(ON_ANIM_NEXT, animQueue.shift());
             $slider.animate({
                 left: 0
             }, ANIM_SPEED, function() {
-                $(document).trigger(ON_ANIM_NEXT);
+                queue.complete();
             });
         }
     };
 
+    function startQueue() {
+
+    };
+
     $(document).ready(function() {
+        var width;
         $(SELECTOR).each(function() {
             var $this = $(this);
-            var width = $this.width();
+            width = width || $this.width();
             var height = $this.height();
 
             var text = $this.text();
@@ -30,8 +83,10 @@
                 .css({
                     height: height,
                     position: "relative",
+//                    visibility: "hidden",
                     _: null
                 })
+                .hide()
                 .html(""); // &nbsp;
 
             var $div = $("<div>").css({
@@ -46,7 +101,7 @@
             ;
             $this.append($div);
 
-            var $slider = $("<div>").css({
+            var $slider = $("<div class='_slider'>").css({
                 position: "absolute",
                 top: 0,
                 left: width,
@@ -58,9 +113,34 @@
             ;
             $div.append($slider);
 
-            animQueue.push(slideQueue($slider));
+            queue.add(function() {
+                $this.show();
+//                $this.css({visibility: "visible"});
+                $slider.animate({
+                    left: 0
+                }, ANIM_SPEED, function() {
+                    queue.complete();
+                });
+            });
         });
-        animQueue && (animQueue.shift())();
+        // add show-delay and fadeout
+        queue.add(function() {
+            var $elems = $(SELECTOR);
+            $elems
+                .delay(SHOW_DELAY)
+                .fadeOut(1000, function() {
+                    var $this = $(this);
+//                    //
+//                    $this.show().css({visibility:"hidden"});
+                    $this.find("._slider").css({left: width});
+                    FADEOUT_COMPLETE_FLAG = (FADEOUT_COMPLETE_FLAG + 1) % $elems.length;
+                    if (FADEOUT_COMPLETE_FLAG == 0) {
+                        queue.complete();
+                    }
+                })
+            ;
+        });
+        queue.start();
 
     });
 })(jQuery);
